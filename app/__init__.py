cbd5334e65a4a60fc7a250a4ff46cc66e8f62292
sami-mai <samirah.maison@gmail>
2018-04-04 11:43:36 +0300

    storing API keys and making API requests

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 from .config import DevConfig
 
 # Initializing application
-app = Flask(__name__)
+# app = Flask(__name__)
 # connect our application with this new configuration file
-# app = Flask(__name__,instance_relative_config = True)
+app = Flask(__name__,instance_relative_config = True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
-# app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py')
 
 from app import views
--- app/request.py
+++ app/request.py
@@ -1,11 +1,11 @@
 
-# from app import app
+from app import app
 # import urllib.request,json
 # from .models import movie
 #
 # Movie = movie.Movie
-# # Getting api key
-# api_key = app.config['MOVIE_API_KEY']
+# Getting api key
+api_key = app.config['MOVIE_API_KEY']
 #
 # # Getting the movie base url
 # base_url = app.config["MOVIE_API_BASE_URL"]
