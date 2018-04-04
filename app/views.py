ba843010b4ae504ccca920e810c924c81c20534e
sami-mai <samirah.maison@gmail>
2018-04-04 14:10:48 +0300

    add more categories and use macros

--- app/views.py
+++ app/views.py
@@ -17,11 +17,11 @@ def index():
     popular_movies = get_movies('popular')
     print(popular_movies)
     # adding more categories
-    # upcoming_movie = get_movies('upcoming')
-    # now_showing_movie = get_movies('now_playing')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
 
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, message = message, popular = popular_movies)#, upcoming = upcoming_movie, now_showing = now_showing_movie)
+    return render_template('index.html', title = title, message = message, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
 # add a new route that have a movie() view function.
 # The part in the angle brackets <> is dynamic.
