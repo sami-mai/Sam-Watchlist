7637e413a730df734a79457dc09014eaf8ed3d97
sami-mai <samirah.maison@gmail>
2018-04-05 10:58:53 +0300

    request object tosearch for movies

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for #The request object is provided by flask and it encapsulates our HTTP request with all its arguments to the view function
 from app import app
-from .request import get_movies,get_movie
+from .request import get_movies,get_movie,search_movie
 
 # Views
 @app.route('/')
@@ -9,7 +9,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hi World!'
+    # message = 'Hi World!'
 
     # return render_template('index.html',message = message)
 
@@ -21,7 +21,13 @@ def index():
     now_showing_movie = get_movies('now_playing')
 
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, message = message, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
+# Request object
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
+        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
 # add a new route that have a movie() view function.
 # The part in the angle brackets <> is dynamic.
@@ -50,3 +56,16 @@ def movie(id):
     title = f'{movie.title}'
 
     return render_template('movie.html',title = title,movie = movie)
+
+
+#create the search view function that will display our search items from the API
+@app.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    View function to display the search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html',movies = searched_movies)
