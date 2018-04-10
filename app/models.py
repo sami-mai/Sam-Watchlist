07ee1abc5a71a7737cb2acd5b1951ea1e2a13ba4
sami-mai <samirah.maison@gmail>
2018-04-10 10:45:42 +0300

    create the user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from . import db
+
+
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -39,3 +42,12 @@ class Review:
                 response.append(review)
 
         return response
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
